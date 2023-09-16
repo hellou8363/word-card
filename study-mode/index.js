5f9493ecc3919ac693578bff253a958b7619bce9
eunhye han <hellou8363@gmail.com>
2023-09-17 00:51:09 +0900

    학습모드 및 Day1, 2 단어 추가

---
+++ study-mode/index.js
@@ -0,0 +1,78 @@
+const selector = (element) => document.querySelector(element);
+const cardLayout = selector(".card");
+const word = selector(".word");
+const meaning = selector(".meaning");
+const wordState = selector(".card .word");
+const selectedDate = selector(".date-selection");
+const wordFilePath = (day) => `../words/${day}.json`;
+
+let wordList = [];
+let currentCardOrder = 0;
+
+fetch(wordFilePath("day1"))
+  .then((response) => response.json())
+  .then((data) => {
+    wordList = Object.entries(data);
+    word.innerText = wordList[0][0];
+    meaning.innerText = wordList[0][1];
+  })
+  .catch((error) => console.log(error));
+
+const callDayFile = (day) => {
+  fetch(wordFilePath(day))
+    .then((response) => response.json())
+    .then((data) => {
+      wordList = Object.entries(data);
+      word.innerText = wordList[0][0];
+      meaning.innerText = wordList[0][1];
+    })
+    .catch((error) => console.log(error));
+};
+
+function wordStyleInit() {
+  meaning.style.display = "none";
+  word.style.display = "block";
+  wordState.className = "word on";
+}
+
+selectedDate.addEventListener("change", () => callDayFile(selectedDate.value));
+
+cardLayout.addEventListener("click", () => {
+  if (wordState.className === "word on") {
+    word.style.display = "none";
+    meaning.style.display = "block";
+    wordState.className = "word";
+  } else {
+    wordStyleInit();
+  }
+});
+
+selector(".btn-left").addEventListener("click", () => {
+  const max = wordList.length;
+
+  wordStyleInit();
+
+  if (currentCardOrder - 1 < 0) {
+    currentCardOrder = max - 1;
+  } else {
+    currentCardOrder--;
+  }
+
+  word.innerText = wordList[currentCardOrder][0];
+  meaning.innerText = wordList[currentCardOrder][1];
+});
+
+selector(".btn-right").addEventListener("click", () => {
+  const max = wordList.length;
+
+  wordStyleInit();
+
+  if (currentCardOrder + 1 >= max) {
+    currentCardOrder = 0;
+  } else {
+    currentCardOrder++;
+  }
+
+  word.innerText = wordList[currentCardOrder][0];
+  meaning.innerText = wordList[currentCardOrder][1];
+});
